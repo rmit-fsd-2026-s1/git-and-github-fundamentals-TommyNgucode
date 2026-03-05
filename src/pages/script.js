8ee00dd5994b6b2403e7d9946bd5f8dbac7a4f22
tommyngucode <s4093392@student.rmit.edu.au>
2026-03-05 20:54:17 +1100

    added input field validation

--- src/pages/script.js
+++ src/pages/script.js
@@ -2,11 +2,14 @@
 
 const form = document.getElementById("form");
 const input = form.querySelectorAll("input");
+
+const phoneField = document.getElementById("phone")
+const emailField = document.getElementById("email")
 const submitBtn = document.getElementById("submitBtn");
 
 form.addEventListener('submit', () => {
     let allFillled = true;
-    input.map(input => {
+    input.forEach(input => {
         if (input.value === '') {
             alert("Please fill out all fields");
             allFillled = false
@@ -19,3 +22,25 @@ form.addEventListener('submit', () => {
 )
 
 
+emailField.addEventListener("input", function() {
+    if (emailField.validity.patternMismatch) {
+        emailField.setCustomValidity("Email must be like: example@gmail.com");
+    }
+    else {
+        emailField.setCustomValidity("");
+    }
+
+    emailField.reportValidity();
+})
+
+
+phoneField.addEventListener("input", function() {
+    if (phoneField.validity.patternMismatch) {
+        phoneField.setCustomValidity("Phone must be like: ### ### ###");
+    }
+    else {
+        phoneField.setCustomValidity("");
+    }
+
+    phoneField.reportValidity();
+})
